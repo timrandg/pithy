624db370e6e1972381d5d50d0306319833ac7825
Tim Rand <tim.rand@tempus.com>
2022-11-01 22:09:54 -0500

    meth module added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pithy',
+    version='0.0.1',
+    description='Pithy is a test package for pip installation via github',
+    url='git@github.com:timrandg/pithy.git',
+    author='Tim Rand',
+    author_email='timrandg@gmail.com',
+    license='unlicensed',
+    packages=['pithy'],
+    zip_safe=False,
+)
